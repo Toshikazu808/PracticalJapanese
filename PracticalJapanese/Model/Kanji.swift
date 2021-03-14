9cabf6b7e2bb5bb7d892a6a9b73d747665f2c1df
Toshikazu808 <65317324+Toshikazu808@users.noreply.github.com>
2021-03-14 10:57:43 -0700

    target iOS changed to 13.0.  app update pushed to app store.

---
+++ PracticalJapanese/Model/Kanji.swift
@@ -0,0 +1,19 @@
+//
+//  Kanji.swift
+//  PracticalJapanese
+//
+//  Created by Ryan Kanno on 2/20/21.
+//  Copyright © 2021 Ryan Kanno. All rights reserved.
+//
+
+import Foundation
+
+struct Kanji {
+    let kanji: String
+    let onYomi: String
+    let kunYomi: String
+    let definition: String
+    let example: String
+} // End of struct Kanji
+
+
